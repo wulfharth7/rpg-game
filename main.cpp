7bdc612d579902e1b17040594ae8014e9b0c296f
wulfharth7 <legendcrafter24@gmail.com>
2022-08-15 14:39:08 +0300

    learning stuff from documents about the mouse cursor

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
     sf::Texture playerTexture;
     sf::Sprite playerSprite;
+
+    sf::RectangleShape bullet(sf::Vector2f(50, 25));
+    bullet.setPosition(sf::Vector2f(100, 100));
 
     if (playerTexture.loadFromFile("assets/player/textures/spritesheet.png")) {
         std::cout << "player image loaded" << std::endl;
@@ -19,9 +24,11 @@ int main()
     else {
         std::cout << "player image couldn't load" << std::endl;
     }
-   
+
+    
     while (window.isOpen())
     {
+
         sf::Vector2f position = playerSprite.getPosition();
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
             sf::Vector2f position = playerSprite.getPosition();
@@ -37,9 +44,15 @@ int main()
             playerSprite.setPosition(position + sf::Vector2f(0, 1));
         }
 
-
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+            sf::Vector2f vec = sf::Vector2f(localPosition);
+            sf::Vector2f direction = vec - bullet.getPosition();
+            bullet.setPosition(bullet.getPosition() + direction*0.001f);
+        }
         window.clear();
         window.draw(playerSprite);
+        window.draw(bullet);
         window.display();
     }
 
