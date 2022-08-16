8a9997997bb30fb961a5abb7a9c24567858c16dc
wulfharth7 <legendcrafter24@gmail.com>
2022-08-16 09:51:51 +0300

    trying out some bullet controls, the code is a mess right now for testing purposes, will clean it soon

--- main.cpp
+++ main.cpp
@@ -9,15 +9,16 @@ int main()
     sf::Sprite playerSprite;
 
     sf::RectangleShape bullet(sf::Vector2f(50, 25));
-    bullet.setPosition(sf::Vector2f(100, 100));
+    bullet.setPosition(sf::Vector2f(50, 50));
+    sf::Vector2f direction;
+    int XIndex = 0;
+    int YIndex = 0;
 
     if (playerTexture.loadFromFile("assets/player/textures/spritesheet.png")) {
         std::cout << "player image loaded" << std::endl;
         playerSprite.setTexture(playerTexture);
 
-        int XIndex = 0;
-        int YIndex = 0;
-
+        
         playerSprite.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
 
     }
@@ -31,24 +32,57 @@ int main()
 
         sf::Vector2f position = playerSprite.getPosition();
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            XIndex = XIndex++;
+            YIndex = 3;
+            if (XIndex >= 8) {
+                XIndex = 0;
+            }
+            playerSprite.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
+
             sf::Vector2f position = playerSprite.getPosition();
-            playerSprite.setPosition(position + sf::Vector2f(1, 0));
-        } if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            playerSprite.setPosition(position + sf::Vector2f(0.075, 0));
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            XIndex = XIndex++;
+            YIndex = 1;
+            if (XIndex >= 8) {
+                XIndex = 0;
+            }
+            playerSprite.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
             sf::Vector2f position = playerSprite.getPosition();
-            playerSprite.setPosition(position + sf::Vector2f(-1, 0));
-        } if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            playerSprite.setPosition(position + sf::Vector2f(-0.075, 0));
+        }else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            XIndex = XIndex++;
+            YIndex = 0;
+            if (XIndex >= 8) {
+                XIndex = 0;
+            }
+            playerSprite.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
             sf::Vector2f position = playerSprite.getPosition();
-            playerSprite.setPosition(position + sf::Vector2f(0, -1));
-        } if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            playerSprite.setPosition(position + sf::Vector2f(0, -0.075));
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            XIndex = XIndex++;
+            YIndex = 2;
+            if (XIndex >= 8) {
+                XIndex = 0;
+            }
+            playerSprite.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
             sf::Vector2f position = playerSprite.getPosition();
-            playerSprite.setPosition(position + sf::Vector2f(0, 1));
+            playerSprite.setPosition(position + sf::Vector2f(0, 0.075));
+        }
+        else {
+            XIndex = 0;
+            playerSprite.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
         }
 
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-            sf::Vector2i localPosition = sf::Mouse::getPosition(window);
-            sf::Vector2f vec = sf::Vector2f(localPosition);
-            sf::Vector2f direction = vec - bullet.getPosition();
-            bullet.setPosition(bullet.getPosition() + direction*0.001f);
+            sf::Vector2f localPosition = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+            //sf::Vector2f vec = sf::Vector2f(localPosition);
+            direction = bullet.getPosition();
+            direction = localPosition - bullet.getPosition();
+            bullet.setPosition(bullet.getPosition() + direction* 0.0003f);
+        }
+        else {
+            bullet.setPosition(bullet.getPosition() + direction * 0.0003f);
         }
         window.clear();
         window.draw(playerSprite);
