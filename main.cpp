d657bea82e354c3e914692d71814e10bd955eb7c
wulfharth7 <legendcrafter24@gmail.com>
2022-08-18 09:30:43 +0300

    trying out some mouse textures, they aren't animated for now

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1700, 800), "SFML works!");
+    window.setMouseCursorVisible(false);
+    sf::Texture texture;
+    texture.loadFromFile("assets/mouse/textures/cursor.png");
+    sf::Sprite sprite(texture);
+    sprite.scale(sf::Vector2f(0.65, 0.65));
+
     player player;
     player.load();
     sf::Clock clock;
@@ -14,11 +20,13 @@ int main()
     window.setFramerateLimit(200);
     while (window.isOpen())
     {
+        sprite.setPosition(static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)));
         timer = clock.restart();
         time = timer.asMilliseconds();
         window.clear(sf::Color::White);
         player.update(window,time);
         player.Draw(window);
+        window.draw(sprite);
         window.display();
     }
 
