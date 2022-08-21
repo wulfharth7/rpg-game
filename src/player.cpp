16715bbc9ae0efed53b0c06b7603bdedd5923a2b
wulfharth7 <legendcrafter24@gmail.com>
2022-08-21 21:14:13 +0300

    cleaned the player class code a bit more

--- src/player.cpp
+++ src/player.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include "bullet.h"
 
-
 int count = 12;
 player::player():
     maxFireRate(250), fireRateTimer(0), spriteSheetRate(100), spriteSheetTimer(0)
@@ -27,23 +26,31 @@ void player::Draw(sf::RenderWindow& window)
 }
 
 void player::update(sf::RenderWindow& window, float time)
-{
-    mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+{  
+    fireRateTimer += time;
     spriteSheetTimer += time;
+    move(time);
+    shoot(time, window);    
+}
+
+void player::load()
+{
+    if (playerTexture.loadFromFile("assets/player/textures/spritesheet.png")) {
+        std::cout << "player image loaded" << std::endl;
+        playerSprite.setTexture(playerTexture);
+        playerSprite.setTextureRect(sf::IntRect(X_spritesheet * 64, Y_spritesheet * 64, 64, 64));
+    }
+    else {
+        std::cout << "player image couldn't load" << std::endl;
+    }
+}
+
+void player::move(float time)
+{
     sf::Vector2f position = playerSprite.getPosition();
-
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-
-
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            if (spriteSheetTimer >= spriteSheetRate) {
-                X_spritesheet = X_spritesheet++;
-                Y_spritesheet = 3;
-                if (X_spritesheet >= 8) {
-                    X_spritesheet = 0;
-                }
-                spriteSheetTimer = 0;
-            }
+            changeSprite('D', time);
             playerSprite.setTextureRect(sf::IntRect(X_spritesheet * 64, Y_spritesheet * 64, 64, 64));
             sf::Vector2f position = playerSprite.getPosition();
             playerSprite.setPosition(position + sf::Vector2f(0.075, 0) * time * player_speed);
@@ -51,15 +58,7 @@ void player::update(sf::RenderWindow& window, float time)
 
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-            if (spriteSheetTimer >= spriteSheetRate) {
-                X_spritesheet = X_spritesheet++;
-                Y_spritesheet = 1;
-                if (X_spritesheet >= 8) {
-                    X_spritesheet = 0;
-                }
-                spriteSheetTimer = 0;
-            }
-
+            changeSprite('A',time);
             playerSprite.setTextureRect(sf::IntRect(X_spritesheet * 64, Y_spritesheet * 64, 64, 64));
             sf::Vector2f position = playerSprite.getPosition();
             playerSprite.setPosition(position + sf::Vector2f(-0.075, 0) * time * player_speed);
@@ -67,14 +66,7 @@ void player::update(sf::RenderWindow& window, float time)
 
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-            if (spriteSheetTimer >= spriteSheetRate) {
-                X_spritesheet = X_spritesheet++;
-                Y_spritesheet = 0;
-                if (X_spritesheet >= 8) {
-                    X_spritesheet = 0;
-                }
-                spriteSheetTimer = 0;
-            }
+            changeSprite('W', time);
             playerSprite.setTextureRect(sf::IntRect(X_spritesheet * 64, Y_spritesheet * 64, 64, 64));
             sf::Vector2f position = playerSprite.getPosition();
             playerSprite.setPosition(position + sf::Vector2f(0, -0.075) * time * player_speed);
@@ -82,31 +74,25 @@ void player::update(sf::RenderWindow& window, float time)
 
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            if (spriteSheetTimer >= spriteSheetRate) {
-                X_spritesheet = X_spritesheet++;
-                Y_spritesheet = 2;
-                if (X_spritesheet >= 8) {
-                    X_spritesheet = 0;
-                }
-                spriteSheetTimer = 0;
-            }
+            changeSprite('S', time);
             playerSprite.setTextureRect(sf::IntRect(X_spritesheet * 64, Y_spritesheet * 64, 64, 64));
             sf::Vector2f position = playerSprite.getPosition();
             playerSprite.setPosition(position + sf::Vector2f(0, 0.075) * time * player_speed);
         }
     }
     else {
-        X_spritesheet = 0;
+        changeSprite('x', time);
         playerSprite.setTextureRect(sf::IntRect(X_spritesheet * 64, Y_spritesheet * 64, 64, 64));
     }
-        
-    
-      
-    
 
-    fireRateTimer += time;
+}
+
+void player::shoot(float time, sf::RenderWindow& window)
+{
+    
     if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && fireRateTimer >= maxFireRate) {
-        bullets.push_back(bullet(10,10,playerSprite.getPosition(),mousePos,count));
+        mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+        bullets.push_back(bullet(10, 10, playerSprite.getPosition(), mousePos, count));
         count--;
         if (count <= 0) {
             count = 0;
@@ -121,18 +107,52 @@ void player::update(sf::RenderWindow& window, float time)
         bullets[i].distance = bullets[i].movementTimer * bullets[i].bullet_speed;
         std::cout << bullets[0].distance << std::endl;
         //check collision
-            
+
     }
 }
 
-void player::load()
+void player::changeSprite(char direction,float time)
 {
-    if (playerTexture.loadFromFile("assets/player/textures/spritesheet.png")) {
-        std::cout << "player image loaded" << std::endl;
-        playerSprite.setTexture(playerTexture);
-        playerSprite.setTextureRect(sf::IntRect(X_spritesheet * 64, Y_spritesheet * 64, 64, 64));
+    
+    if (direction == 'W') {
+        if (spriteSheetTimer >= spriteSheetRate) {
+            X_spritesheet = X_spritesheet++;
+            Y_spritesheet = 0;
+            if (X_spritesheet >= 8) {
+                X_spritesheet = 0;
+            }
+            spriteSheetTimer = 0;
+        }
+    }else if (direction == 'A') {
+        if (spriteSheetTimer >= spriteSheetRate) {
+            X_spritesheet = X_spritesheet++;
+            Y_spritesheet = 1;
+            if (X_spritesheet >= 8) {
+                X_spritesheet = 0;
+            }
+            spriteSheetTimer = 0;
+        }
+    }else if (direction == 'S') {
+        if (spriteSheetTimer >= spriteSheetRate) {
+            X_spritesheet = X_spritesheet++;
+            Y_spritesheet = 2;
+            if (X_spritesheet >= 8) {
+                X_spritesheet = 0;
+            }
+            spriteSheetTimer = 0;
+        }
+    }else if (direction == 'D') {
+        if (spriteSheetTimer >= spriteSheetRate) {
+            X_spritesheet = X_spritesheet++;
+            Y_spritesheet = 3;
+            if (X_spritesheet >= 8) {
+                X_spritesheet = 0;
+            }
+            spriteSheetTimer = 0;
+        }
     }
     else {
-        std::cout << "player image couldn't load" << std::endl;
+        X_spritesheet = 0;
     }
+
 }
--- src/player.h
+++ src/player.h
@@ -11,6 +11,10 @@ class player
 		void Draw(sf::RenderWindow& window);
 		void update(sf::RenderWindow& window, float time);
 		void load();
+		void move(float time);
+		void shoot(float time, sf::RenderWindow& window);
+		void changeSprite(char direction, float time);
+
 		float player_speed = 2.5f; //i'll make it private
 		sf::Vector2f mousePos;
 
